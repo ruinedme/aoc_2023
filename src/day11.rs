31aa09af0e9d649b25fb5fdab8f661ba8f06e37c
ruinedme <johndoe@example.com>
2023-12-11 23:29:05 -0600

    refactor day11

--- src/day11.rs
+++ src/day11.rs
@@ -19,42 +19,33 @@ pub fn run_day11(inputs: &str) {
 
 fn day11_1(inputs: &str) -> usize {
     let grid = Grid::new(inputs);
-    let scalar = 2;    
-    let mut total = 0;
+    let scalar = 2;
 
     // Get galaxy co-ords
     let mut galaxies = grid.find_all(&b'#');
     // "expand" the universe
     galaxies = expand_galaxies(galaxies, &grid, scalar);
-    
-    let pairs: HashSet<(usize, usize)> = get_pairs(&galaxies);
-    for (a, b) in pairs {
-        total += grid.manhattan_distance(&galaxies[a], &galaxies[b]);
-    }
 
-    return total;
+    return sum_distances(&galaxies, &grid);
 }
 
 fn day11_2(inputs: &str) -> usize {
     let grid = Grid::new(inputs);
     let scalar = 1_000_000;
-    let mut total = 0;
 
     // Get galaxy co-ords
     let mut galaxies = grid.find_all(&b'#');
     // "expand" the universe
     galaxies = expand_galaxies(galaxies, &grid, scalar);
 
-    let pairs = get_pairs(&galaxies);
-
-    for (a, b) in &pairs {
-        total += grid.manhattan_distance(&galaxies[*a], &galaxies[*b]);
-    }
-
-    return total;
+    return sum_distances(&galaxies, &grid);
 }
 
-fn expand_galaxies(galaxies: Vec<(usize,usize)>, grid: &Grid, scalar: isize) -> Vec<(usize,usize)> {
+fn expand_galaxies(
+    galaxies: Vec<(usize, usize)>,
+    grid: &Grid,
+    scalar: isize,
+) -> Vec<(usize, usize)> {
     let mut empty_cols: Vec<usize> = Vec::with_capacity(grid.width());
     let empty_rows: Vec<usize> = grid
         .map
@@ -80,14 +71,12 @@ fn expand_galaxies(galaxies: Vec<(usize,usize)>, grid: &Grid, scalar: isize) -> 
     return galaxies
         .iter()
         .map(|(y, x)| {
-            let crossed_cols: isize =
-                empty_cols
-                    .iter()
-                    .fold(0, |acc, cx| if cx < x { acc + 1 } else { acc });
-            let crossed_rows: isize =
-                empty_rows
-                    .iter()
-                    .fold(0, |acc, cy| if cy < y { acc + 1 } else { acc });
+            let crossed_cols: isize = empty_cols
+                .iter()
+                .fold(0, |acc, cx| if cx < x { acc + 1 } else { acc });
+            let crossed_rows: isize = empty_rows
+                .iter()
+                .fold(0, |acc, cy| if cy < y { acc + 1 } else { acc });
 
             let dx = (crossed_cols * (scalar - 1)).max(0) as usize;
             let dy = (crossed_rows * (scalar - 1)).max(0) as usize;
@@ -96,16 +85,16 @@ fn expand_galaxies(galaxies: Vec<(usize,usize)>, grid: &Grid, scalar: isize) -> 
         .collect();
 }
 
-fn get_pairs(galaxies: &Vec<(usize,usize)>) -> HashSet<(usize, usize)> {
-    // better way to do this?
-    let mut pairs: HashSet<(usize, usize)> = HashSet::new();
+fn sum_distances(galaxies: &Vec<(usize, usize)>, grid: &Grid) -> usize {
+    let mut total = 0;
+
     for i in 0..galaxies.len() {
-        for j in 1..galaxies.len() {
-            if i != j && !pairs.contains(&(j, i)) {
-                pairs.insert((i, j));
+        for j in i..galaxies.len() {
+            if i != j {
+                total += grid.manhattan_distance(&galaxies[i], &galaxies[j]);
             }
         }
     }
 
-    return pairs;
+    return total;
 }
