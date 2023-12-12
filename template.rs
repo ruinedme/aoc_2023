a0e5f068a971efae708050fde1a893e98d420d72
ruinedme <johndoe@example.com>
2023-12-12 07:15:56 -0600

    update template

--- template.rs
+++ template.rs
@@ -4,12 +4,12 @@ use timer::profile;
 
 pub fn run_day{day}(inputs: &str) {
     profile! {
-        let day{day}_1 = day{day}_1(&inputs);
+        let day{day}_1 = day{day}_1(inputs);
         println!("Day {day}-1: {day{day}_1}");
     }
 
     profile! {
-        let day{day}_2 = day{day}_2(&inputs);
+        let day{day}_2 = day{day}_2(inputs);
         println!("Day {day}-2: {day{day}_2}");
     }
 }
