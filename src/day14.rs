07cdb0e8dd294ca682ea048461e8f7ccf6b0bc6a
ruinedme <johndoe@example.com>
2023-12-14 18:22:17 -0600

    fix formatting

--- src/day14.rs
+++ src/day14.rs
@@ -19,21 +19,24 @@ fn day14_1(inputs: &str) -> usize {
     let grid = Grid::new(inputs);
     let mut rocks = grid.find_all(&b'O');
 
-    rocks = rocks.iter().map(|rock| {
-        let mut y = rock.0;
-        let mut boulders = 0;
-        while y > 0{
-            match grid.map[y - 1][rock.1] {
-                b'O' => boulders += 1,
-                b'#' => break,
-                _ => (),
+    rocks = rocks
+        .iter()
+        .map(|rock| {
+            let mut y = rock.0;
+            let mut boulders = 0;
+            while y > 0 {
+                match grid.map[y - 1][rock.1] {
+                    b'O' => boulders += 1,
+                    b'#' => break,
+                    _ => (),
+                }
+                y -= 1;
             }
-            y -= 1;
-        }
-        (y+boulders,rock.1)
-    }).collect();
+            (y + boulders, rock.1)
+        })
+        .collect();
 
-    return rocks.iter().map(|(y,_)| grid.height() - y).sum();
+    return rocks.iter().map(|(y, _)| grid.height() - y).sum();
 }
 
 fn day14_2(inputs: &str) -> usize {
@@ -44,80 +47,89 @@ fn day14_2(inputs: &str) -> usize {
     let cycles = 1000;
     for _ in 0..cycles {
         let mut rocks = grid.find_all(&b'O');
-        
+
         // tilt north
-        rocks = rocks.iter().map(|rock| {
-            let mut y = rock.0;
-            let mut boulders = 0;
-            while y > 0 {
-                match grid.map[y-1][rock.1] {
-                    b'O' => boulders += 1,
-                    b'#' => break,
-                    _=> (),
+        rocks = rocks
+            .iter()
+            .map(|rock| {
+                let mut y = rock.0;
+                let mut boulders = 0;
+                while y > 0 {
+                    match grid.map[y - 1][rock.1] {
+                        b'O' => boulders += 1,
+                        b'#' => break,
+                        _ => (),
+                    }
+                    y -= 1;
                 }
-                y -= 1;
-            }
-            grid.map[rock.0][rock.1] = b'.';
-            grid.map[y+boulders][rock.1] = b'O';
-            (y+boulders,rock.1)
-        }).collect();
+                grid.map[rock.0][rock.1] = b'.';
+                grid.map[y + boulders][rock.1] = b'O';
+                (y + boulders, rock.1)
+            })
+            .collect();
         rocks.sort();
 
         //tilt west
-        rocks = rocks.iter().map(|rock| {
-            let mut x = rock.1;
-            let mut boulders = 0;
-            while x > 0 {
-                match grid.map[rock.0][x-1] {
-                    b'O' => boulders += 1,
-                    b'#' => break,
-                    _=> (),
+        rocks = rocks
+            .iter()
+            .map(|rock| {
+                let mut x = rock.1;
+                let mut boulders = 0;
+                while x > 0 {
+                    match grid.map[rock.0][x - 1] {
+                        b'O' => boulders += 1,
+                        b'#' => break,
+                        _ => (),
+                    }
+                    x -= 1;
                 }
-                x -= 1;
-            }
 
-            grid.map[rock.0][rock.1] = b'.';
-            grid.map[rock.0][x+boulders] = b'O';
-            (rock.0,x+boulders)
-        }).collect();
+                grid.map[rock.0][rock.1] = b'.';
+                grid.map[rock.0][x + boulders] = b'O';
+                (rock.0, x + boulders)
+            })
+            .collect();
         rocks.sort();
         rocks.reverse();
-        
+
         //tilt south
-        rocks = rocks.iter().map(|rock| {
-            let mut y = rock.0;
-            let mut boulders = 0;
-            while y < grid.height() - 1 {
-                match grid.map[y+1][rock.1] {
-                    b'O' => boulders += 1,
-                    b'#' => break,
-                    _=> (),
+        rocks = rocks
+            .iter()
+            .map(|rock| {
+                let mut y = rock.0;
+                let mut boulders = 0;
+                while y < grid.height() - 1 {
+                    match grid.map[y + 1][rock.1] {
+                        b'O' => boulders += 1,
+                        b'#' => break,
+                        _ => (),
+                    }
+                    y += 1;
                 }
-                y += 1;
-            }
-            grid.map[rock.0][rock.1] = b'.';
-            grid.map[y-boulders][rock.1] = b'O';
-            (y-boulders,rock.1)
-        }).collect();
+                grid.map[rock.0][rock.1] = b'.';
+                grid.map[y - boulders][rock.1] = b'O';
+                (y - boulders, rock.1)
+            })
+            .collect();
 
         rocks.sort();
         rocks.reverse();
-        
+
         //tilt east
         rocks.iter().for_each(|rock| {
             let mut x = rock.1;
             let mut boulders = 0;
             while x < grid.width() - 1 {
-                match grid.map[rock.0][x+1] {
+                match grid.map[rock.0][x + 1] {
                     b'O' => boulders += 1,
                     b'#' => break,
-                    _=> (),
+                    _ => (),
                 }
                 x += 1;
             }
-            
+
             grid.map[rock.0][rock.1] = b'.';
-            grid.map[rock.0][x-boulders] = b'O';
+            grid.map[rock.0][x - boulders] = b'O';
             // (rock.0,x-boulders)
         });
 
@@ -127,11 +139,11 @@ fn day14_2(inputs: &str) -> usize {
             let final_index = index + (1_000_000_000 - index) % cycle_len;
             grid.map = sets[final_index].clone();
             let final_rocks = grid.find_all(&b'O');
-            return final_rocks.iter().map(|(y,_)| grid.height() - y).sum();
+            return final_rocks.iter().map(|(y, _)| grid.height() - y).sum();
         }
         sets.push(grid.map.clone());
     }
 
     let rocks = grid.find_all(&b'O');
-    return rocks.iter().map(|(y,_)| grid.height() - y).sum();
+    return rocks.iter().map(|(y, _)| grid.height() - y).sum();
 }
